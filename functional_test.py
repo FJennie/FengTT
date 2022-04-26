6929a6701c1c487cad6bfb453e4c05d6b1255c0f
ftt <2681296210@qq.com>
2022-04-26 15:07:16 +0800

    Refactor home page view to use a template

--- functional_test.py
+++ functional_test.py
@@ -15,11 +15,6 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.quit()
 
     def test_can_start_a_list_and_retrieve_it_later(self):
-        self.browser.get('http://localhost:8000')
-        self.assertIn('To-Do', self.browser.title), "Browser title was:" + self.browser.title
-        self.fail('Finish the test!')
-
-    def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
         self.browser.get('http://localhost:8000')
