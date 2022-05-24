02159348a9e42c94c32dcd2b62d5dfaca645acf9
ftt <2681296210@qq.com>
2022-05-24 08:39:05 +0800

    make functional_test an app, use LiveServerTestCase

--- functional_test/tests.py
+++ functional_test/tests.py
@@ -1,13 +1,13 @@
 import time
 
 from selenium import webdriver
-import unittest
 
 from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
 
+from django.test import LiveServerTestCase
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Chrome(executable_path=r'D:\Google Driver\chromedriver.exe')
 
@@ -22,14 +22,14 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
         # She notices the page title and header mention to - do lists
         self.assertIn('To-Do', self.browser.title)
         header_text = self.browser.find_element(By.TAG_NAME, 'h1').text
         self.assertIn('To-Do', header_text)
         # She is invited to enter a to-do item straight away
         inputbox = self.browser.find_element(By.ID, 'id_new_item')
-        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys('Buy peacock feathers')
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to - do list
         inputbox.send_keys(Keys.ENTER)
@@ -47,25 +47,3 @@ class NewVisitorTest(unittest.TestCase):
         self.check_for_row_in_list_table('1: Buy peacock feathers')
         self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
         self.fail( 'Finish the test!')
-
-if __name__ == '__main__':
-    unittest.main(warnings='ignore')
-# Edith has heard about a cool new online to-do app. She goes
-# to check out its homepage
-# She notices the page title and header mention to-do lists
-
-# She is invited to enter a to-do item straight away
-# She types "Buy peacock feathers" into a text box (Edith's hobby
-# is tying fly-fishing lures)
-# When she hits enter, the page updates, and now the page lists
-# "1: Buy peacock feathers" as an item in a to-do list
-# There is still a text box inviting her to add another item. She
-# enters "Use peacock feathers to make a fly" (Edith is very methodical)
-# The page updates again, and now shows both items on her list
-# Edith wonders whether the site will remember her list. Then she sees
-# that the site has generated a unique URL for her -- there is some
-# explanatory text to that effect.
-# She visits that URL - her to-do list is still there.
-# Satisfied, she goes back to sleep
-
-# self.browser = webdriver.Chrome(executable_path=r'D:\Google Driver\chromedriver.exe') # 浏览器驱动
