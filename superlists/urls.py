80dfedb6fcfce70e133eab340aba6cc218bdfde9
ftt <2681296210@qq.com>
2022-05-31 13:40:31 +0800

    five changed files, URL for forms x2, moved code in views + tests, new URL

--- superlists/urls.py
+++ superlists/urls.py
@@ -20,4 +20,5 @@ from list import views
 urlpatterns = [
     path('', views.home_page, name='home'),
     path('list/the-only-list-in-the-world/', views.view_list, name='view_list'),
+    path('list/new', views.new_list, name='new_list'),
 ]
